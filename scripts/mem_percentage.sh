9e2372bdfad54b288e7d1cd9cbe926d5c1c84420
laomafeima <laomafeima@gmail.com>
2018-11-24 10:35:48 +0800

    feat: Modify the number of decimal places.

--- scripts/mem_percentage.sh
+++ scripts/mem_percentage.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_mem_percentage() {
-	free -m|grep Mem| awk -F' ' '{printf("%.2f%%",($2-$7)/$2*100)}'
+	free -m|grep Mem| awk -F' ' '{printf("%.1f%%",($2-$7)/$2*100)}'
 }
 
 main() {
